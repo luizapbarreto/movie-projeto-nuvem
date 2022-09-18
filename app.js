85a0e8349994d36d1cd0726a99879a73bac948ee
Luiza <luizapaesbarreto@gmail.com>
2022-09-17 22:27:38 -0300

    atualização 2.0

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
